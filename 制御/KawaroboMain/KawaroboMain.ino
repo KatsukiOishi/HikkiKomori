b675e0ebef9ab747f8062125e03c0661cdf8e886
KatsukiOishi <katsukioishi@icloud.com>
2023-08-27 00:13:14 +0900

    point : 本番前

--- 制御/KawaroboMain/KawaroboMain.ino
+++ 制御/KawaroboMain/KawaroboMain.ino
@@ -6,7 +6,7 @@ static unsigned int res[7];
 static int sig[7];
 static int rr_speed;
 static int rl_speed;
-static bool sig_flug[7],sigloop_flag;
+static bool sig_flug[7],discon_flug;
 
 volatile uint8_t *resport = &PIND;
 
@@ -15,9 +15,19 @@ MD RRMD(RR_EN,RR_RPWM,RR_LPWM,RR_IS1,RR_IS2);
 MD RLMD(RL_EN,RL_RPWM,RL_LPWM,RL_IS1,RL_IS2);
 
 void GPIO_Init();
+void all_signal_zero(){
+	sig[0] = 0;
+	sig[1] = 0;
+	sig[3] = 0;
+	sig[4] = 0;
+}
+void LED_Set(uint8_t p,uint8_t e){
+	digitalWrite(PR_LED,p);
+	digitalWrite(ERR_LED,e);
+}
 
-void buz(uint8_t count,uint8_t ontime,uint8_t offtime){
-	for(uint8_t i; i < count ;i++){
+void buz(uint8_t count,uint16_t ontime,uint16_t offtime){
+	for(uint8_t i=0; i < count ;i++){
 		digitalWrite(BUZ,HIGH);
 		delay(ontime);
 		digitalWrite(BUZ,LOW);
@@ -27,49 +37,94 @@ void buz(uint8_t count,uint8_t ontime,uint8_t offtime){
 
 void setup(){
 	GPIO_Init();
-	allvalReset();
-	Serial.begin(115200);
 	DDRD = 0x00;
-
-	
+	LED_Set(0,1);
+	Serial.begin(115200);
+	AMD.Reset();
+	RRMD.Reset();
+	RLMD.Reset();
+	LED_Set(1,0);
 }
 
 void loop(){
-  while(1){
-		while((PIND | 0B10111111) == 0xFF);
-		while((PIND & 0B01000000) == 0x00)time_base = micros();
-		while((PIND & 0B01111111) != 0x00){
-			for(int i = 0; i < 6; i++){
-				if((PIND & _BV(i)) == LOW){
-					if(sig_flug[i] == false){
-						time_count[i] = micros();
-						sig_flug[i] = true;
-					}
+	for(uint8_t i=0; i<6; i++){
+		time_count[i]=0;
+		sig[i]=0;
+		sig_flug[i]=false;
+		sig[i]=0;
+	}
+	time_base=0;
+	time_buf=0;
+
+	while((PIND | 0B10111111) == 0xFF);
+	while((PIND & 0B01000000) == 0x00)time_base = micros();
+	while((PIND & 0B01111111) != 0x00){
+		for(uint8_t i = 0; i < 6; i++){
+			if((PIND & _BV(i)) == LOW){
+				if(sig_flug[i] == false){
+					time_count[i] = micros();
+					sig_flug[i] = true;
 				}
 			}
 		}
-		time_buf = micros();
-		for(int i = 1; i < 6; i++){
-			if(time_count[i] == 0)time_count[i] = time_buf;
-			res[i] = time_count[i] - time_base;
-			if(res[i] < 900 || res[i] > 2500)sigloop_flag = true;
-		}
-		if(sigloop_flag == false)break;
-		allvalReset();
 	}
-	sig[0]=constrain(map(res[3], 1350, 1720, -255, 255), -255, 255);
-	sig[1]=constrain(map(res[5], 1350, 1720, -255, 255), -255, 255);
+	time_buf = micros();
+	for(uint8_t i = 1; i < 6; i++){
+		if(time_count[i] == 0)time_count[i] = time_buf;
+		res[i] = time_count[i] - time_base;
+	}
+	
+	sig[0]=constrain(map(res[5], 1350, 1720, -255, 255), -255, 255);
+	sig[1]=constrain(map(res[3], 1350, 1720, -255, 255), -255, 255);
 	sig[2]=constrain(map(res[4], 1135, 1930, -255, 255), -255, 255);
 	sig[3]=constrain(map(res[1], 1130, 1930, -255, 255), -255, 255);
 	sig[4]=constrain(map(res[2], 1130, 1930, -255, 255), -255, 255);
-	for(int i = 0; i < 5; i++)if(sig[i] <= 40 && sig[i] >= -40) sig[i] = 0;
-	
-	if((sig[3] > 0)&&(digitalRead(ULS) == 1)) sig[3]=0;
-	if((sig[3] < 0)&&(digitalRead(DLS) == 1)) sig[3]=0;
 
-	RLMD.Rotate(sig[0],255);
-	RRMD.Rotate(sig[1],255);
-	AMD.Rotate(sig[3],255,1);
+	for(uint8_t i = 0; i < 5; i++){
+		if(sig[i] <= 40 && sig[i] >= -40) sig[i] = 0;
+	}
+	
+	if((sig[3] > 0)&&(digitalRead(ULS) == 1)) sig[3] = 0;
+	if((sig[3] < 0)&&(digitalRead(DLS) == 1)) sig[3] = 0;
+
+	if(sig[2] < -220){
+		discon_flug = true;
+		AMD.DriverDisable();
+		RLMD.DriverDisable();
+		RRMD.DriverDisable();
+		all_signal_zero();
+		buz(1,500,10);
+		delay(1000);
+		LED_Set(0,1);
+	}else{
+		if(discon_flug == true){
+			buz(3,100,10);
+			discon_flug = false;
+		}
+		LED_Set(1,0);
+	}
+
+	if(sig[4] >= 150){
+		RLMD.Rotate(sig[0],255);
+		RRMD.Rotate(sig[1],255);
+		AMD.Rotate(sig[3],255,1);
+		digitalWrite(BUZ,LOW);
+	}else if(sig[4] <= -150){
+		RLMD.DriverDisable();
+		RRMD.DriverDisable();
+		if(digitalRead(ULS) == 0){
+			digitalWrite(BUZ,HIGH);
+			AMD.Rotate(50,50,1);
+		}else{
+			digitalWrite(BUZ,LOW);
+			AMD.HardStop();
+		}
+	}else{
+		digitalWrite(BUZ,LOW);
+		AMD.DriverDisable();
+		RLMD.DriverDisable();
+		RRMD.DriverDisable();
+	}
 
 	Serial.print("a:");
 	Serial.print(sig[0]);
@@ -86,19 +141,6 @@ void loop(){
 	Serial.print(digitalRead(DLS));
 	Serial.print(digitalRead(HOME_SW));
 	Serial.println();
-	allvalReset();
-}
-
-void allvalReset(){
-	for(int i=0; i<6; i++){
-		time_count[i]=0;
-		sig[i]=0;
-		sig_flug[i]=false;
-		sig[i]=0;
-	}
-	time_base=0;
-	time_buf=0;
-	sigloop_flag=false;
 }
 
 void GPIO_Init(){
